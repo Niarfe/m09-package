7df720bb745e4554c620b7610f36b0c9ad758fbd
Niarfe <efrain.olivares@gmail.com>
2025-03-22 00:24:36 -0700

    done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name="Demo",
+        version='0.1',
+        description='A simple demonstration package',
+        url='http://github.com/Niarfe/m09-package',
+        author='Efrain Olivares',
+        licence='MIT',
+        packages=['demo']
+        )
